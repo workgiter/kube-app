7b8f4faea529c05e5eb4d1866ed37f8072c1fbcc
root <root@CIC001355.localdomain>
2023-01-04 14:45:22 +0000

    Header Bar With App Name

--- client-kube/src/App.js
+++ client-kube/src/App.js
@@ -1,10 +1,19 @@
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Kubernetes App Java</h1>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+          <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+            Kubernetes App Java
+          </Typography>
+      </AppBar>
+      </Box>
     </div>
   );
 }
