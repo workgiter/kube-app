ef251abeeedb1fdaa61112aac7458d28e14d0050
Thomas <workthomas893@gmail.com>
2023-01-05 17:29:11 +0000

    added spring sever

---
+++ server-kube/src/main/java/work/thomas/serverkube/ServerKubeApplication.java
@@ -0,0 +1,14 @@
+package work.thomas.serverkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerKubeApplication {
+
+	public static void main(String[] args) {
+		// System.out.println("asdfasdfsdfsdf-------------------------------------asdfasdf");
+		SpringApplication.run(ServerKubeApplication.class, args);
+	}
+
+}
